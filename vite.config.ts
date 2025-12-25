d045dd798f3c3d4a249b26ad206d84f665e7fa8b
Ph1lip0 <157806100+Ph1lip0@users.noreply.github.com>
2025-12-25 23:23:44 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
   server: {
     proxy: {
       '/api': {
         target: 'https://api.football-data.org',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+        rewrite: (pathStr) => pathStr.replace(/^\/api/, ''),
         headers: {
           'X-Auth-Token': '9170ec64b5034fe8986cefe145d52b51',
         },
       },
     },
   },
-});
+})
